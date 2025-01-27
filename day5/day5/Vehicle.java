cc4de99f7607cccc6f460441b54f689a597515ce
Rahulkaushik01 <kaushikrahul725@gmail.com>
2025-01-27 22:03:17 +0530

    Update Vehicle.java

--- day5/day5/Vehicle.java
+++ day5/day5/Vehicle.java
@@ -1,1 +1,31 @@
 
+class bankAccount {
+    int balance = 0;
+    int accountNumber;
+    String accountHolder;
+
+    public int deposit(int x) {
+        balance = balance + x;
+        return balance;
+    }
+
+    public int withdraw(int y)
+    {
+
+        balance =balance -y;
+        return balance;
+    }
+    public int checkBalnce(){
+        return balance;
+    }
+
+
+}
+class Main{
+    public static void main(String [] args){
+        bankAccount Bank = new bankAccount();
+        System.out.println(Bank.deposit(5000));
+        System.out.println(Bank.withdraw(2000));
+        System.out.println(Bank.checkBalnce());
+    }
+}
