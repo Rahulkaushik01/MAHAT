d6b0747247dec6207f891c695ab40a1d9095ac19
Rahulkaushik01 <kaushikrahul725@gmail.com>
2025-01-27 22:04:20 +0530

    Update Book.java

--- day5/day5/Book.java
+++ day5/day5/Book.java
@@ -1,1 +1,22 @@
 
+
+class Book {
+    String title, author, publisher;
+    int ISBN_price;
+
+
+    public int applyDiscount(){
+        int new_price =  ISBN_price - (int)(ISBN_price *0.20);
+        return new_price;
+    }
+
+    public static void main(String[] args) {
+        Book book = new Book();
+        book.ISBN_price = 300;
+        book.title = "FIRE";
+        book.author = "JAMES GOSLING";
+        book.publisher = "NEW_COMPANY";
+        System.out.println(book.applyDiscount());
+
+    }
+}
