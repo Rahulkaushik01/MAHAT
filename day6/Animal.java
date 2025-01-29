968f2c81720634e4ea7556d8262f5d5219c456e2
Rahulkaushik01 <kaushikrahul725@gmail.com>
2025-01-29 21:45:20 +0530

    Update Animal.java

--- day6/Animal.java
+++ day6/Animal.java
@@ -1,1 +1,24 @@
 
+public class Animal{
+    String name , species;
+    Animal(String name, String species){
+        this.name = name;
+        this.species = species;
+
+    }
+
+}
+public class Dog extends Animal{
+    String breed;
+    Dog(String name, String species, String breed){
+        super(name, species);
+        this.breed = breed;
+    }
+    }
+
+
+
+class Mains{
+   public static void main(String[] args){
+   }
+}
