de12736726e1e8d415d71539b0ef7debe62484e6
Rahulkaushik01 <kaushikrahul725@gmail.com>
2025-01-29 21:44:37 +0530

    Update Main.java

--- day6/Main.java
+++ day6/Main.java
@@ -1,1 +1,23 @@
 
+class Person{
+    String name;
+    int age;
+    Person(){
+        name = "Unknown";
+        age = 0;
+
+    }
+    Person(int age, String name){
+        this.name = name;
+        this.age = age;
+
+    }
+}
+class Main {
+
+    public static void main(String[] args) {
+    Person pp = new Person(22, "Rahul");
+    System.out.println(pp.age);
+    System.out.println(pp.name);
+    }
+}
